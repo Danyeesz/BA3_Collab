931130f3862239d4a37f930f2ec7e518e400ce34
Danyeesz <daniel12094@gmail.com>
2021-12-16 20:23:04 +0100

    Recent Changes

--- BA3 Collab/Assets/Daniel/PlayerController_Daniel.cs
+++ BA3 Collab/Assets/Daniel/PlayerController_Daniel.cs
@@ -6,29 +6,79 @@ using UnityEngine.InputSystem;
 public class PlayerController_Daniel : MonoBehaviour
 {
 
-    PlayerControls control;
+    
     Vector2 move;
+    public Vector3 velocity;
+    float gravity = -9.81f;
+    public bool isGrounded;
+    public bool rightHandGrab;
+    public GameObject groundC;
+    public LayerMask ground;
+    public LayerMask objects;
     CharacterController player;
+    PlayerControls control;
+    Transform armature;
+    Transform hips;
+    public Transform righthandpos;
+    Transform lefthandpos;
+    Rigidbody rightHand;
+    Rigidbody leftHAnd;
+    Rigidbody hipsr;
 
     void Awake()
     {
         control = new PlayerControls();
         player = GetComponent<CharacterController>();
+        armature = transform.GetChild(0);
+        hips = armature.GetChild(0);
+        hipsr = hips.GetComponent<Rigidbody>();
+
+        rightHand = GameObject.Find("RightGrab").GetComponent<Rigidbody>();
+        rightHand = GameObject.Find("LeftGrab").GetComponent<Rigidbody>();
 
         control.Movement.Walk.performed += ctx => move = ctx.ReadValue<Vector2>();
-        //control.Movement.Walk.canceled += ctx => move = Vector2.zero;
+        control.Movement.Jump.performed += ctx => Jump();
+        control.Movement.Grab.performed += ctx => Grab();
+        
     }
 
     private void OnEnable()
     {
-
         control.Movement.Enable();
-
     }
 
     private void OnDisable()
     {
         control.Movement.Disable();
+    }
+
+    void Jump() 
+    {
+
+        if (isGrounded == true)
+        {
+            velocity.y = Mathf.Sqrt((2f * -2f * gravity));
+            isGrounded = false;
+            hipsr.AddForce(new Vector3(0, 6000, 0));
+        }
+
+    }
+
+    void Move()
+    {
+        
+        hipsr.AddForce(new Vector3(move.x*60, 0, move.y*60));
+    }
+
+    void Grab()
+    {
+
+        rightHandGrab = Physics.CheckSphere(righthandpos.transform.position, 1.0f, objects);
+        if (rightHandGrab==true)
+        {
+            Debug.Log("GRab");
+        }
+
     }
 
     void Rotation()
@@ -46,8 +96,9 @@ public class PlayerController_Daniel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        player.Move(new Vector3(move.x, 0, move.y)*Time.deltaTime);
+        
         Rotation();
+        isGrounded = Physics.CheckSphere(groundC.transform.position, 0.4f,ground);
 
     }
 }
